1b2408807a27636937af91c314a0755ec018df22
Don Binu <150541671+dbinu63@users.noreply.github.com>
2026-05-14 17:28:47 -0500

    search v2

--- PKSC/week3/assignments/search_array.c
+++ PKSC/week3/assignments/search_array.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    int in, num, arr[100];
-    int temp = 0, i, j;
+    int num, arr[100];
+    int temp, i, j;
 
     printf("Enter the number of inputs: ");
     scanf("%d", &num);
 
     printf("Enter %d inputs: ", num);
-    while (temp < num) {
-        scanf("%d", &in);
-        arr[temp] = in;
-        temp++;
+    for (temp = 0; temp < num; temp++) {
+        scanf("%d", &arr[temp]);
     }
 
-    printf("Enter the number you would like to search for");
-    scanf("%d", &num);
+    printf("Enter the number you would like to search for: ");
+    scanf("%d", &j);
 
-    printf("Would you like to print the first instance, or all instances of the found search term?");
-    printf("\n1 - First instance only\n2 - All instances");
+    printf("Would you like to print the first instance, or all instances of the found search term? ");
+    printf("\n1 - First instance only\n2 - All instances\n: ");
     scanf("%d", &temp);
 
-    if (temp != 1 || temp != 2) {
+    if (temp != 1 && temp != 2) {
         return 0;
     }
 
     for (i = 0; i < num; i++) {
-        if (arr[i] == num) {
-            printf("%d at position: %d", num, i + 1);
-            if (temp == 2) {
+        if (arr[i] == j) {
+            printf("%d at position: %d\n", j, i + 1);
+            if (temp == 1) {
                 return 0;
             }
         }

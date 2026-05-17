96ae78f9793c6bc4fc7ef7d34a463e73d17a2d49
Don Binu <150541671+dbinu63@users.noreply.github.com>
2026-05-16 21:12:51 -0500

    completed clockwise and anti-clockwise matrix row rotation

---
+++ PKSC/week4/assignments/matrices/rotations/rows/clockwise.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int a[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    int i, j, k, num;
+
+    printf("Current: \n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nHow many moves would you like to make: ");
+    scanf("%d", &num);
+
+    for (i = 2; i >= 0; i--) {
+        for (k = 0; k < num; k++) {
+            for (j = 0; j < 3; j++) {
+                a[i][j + 1] += a[i][j];
+                a[i][j] = a[i][j + 1] - a[i][j];
+                a[i][j + 1] -= a[i][j];
+            }
+        }
+    }
+
+    printf("Result: \n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}

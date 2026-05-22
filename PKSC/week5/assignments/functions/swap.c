42c69cea4f9c7ca22372bc6770f6764c71401f4f
Don Binu <150541671+dbinu63@users.noreply.github.com>
2026-05-22 18:21:30 -0500

    completed swap by reference program

---
+++ PKSC/week5/assignments/functions/swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void swap(int *, int *);
+
+int main() {
+    int a = 5, b = 7;
+
+    printf("a: %d\nb: %d\n\n", a, b);
+
+    swap(&a, &b);
+}
+
+void swap(int *a, int *b) {
+    *a += *b;
+    *b = *a - *b;
+    *a -= *b;
+
+    printf("a: %d\nb: %d", *a, *b);
+}

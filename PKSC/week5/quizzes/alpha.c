aa085c3dbae9e57f2072fc27ca854c890f4d1633
Don Binu <150541671+dbinu63@users.noreply.github.com>
2026-05-26 15:18:16 -0500

    alpha.c commit

---
+++ PKSC/week5/quizzes/alpha.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void upper(char in) {
+    if (in > 96 && in < 123) {
+        printf("Result: %c", in - 32);
+    }
+}
+
+void lower(char in) {
+    if (in > 64 && in < 91) {
+        printf("Result: %c", in + 32);
+    }
+}
+
+int main() {
+    int op;
+    char in;
+
+    printf("What char would you like to convert: ");
+    scanf("%c", &in);
+
+    upper(in);
+    lower(in);
+}
